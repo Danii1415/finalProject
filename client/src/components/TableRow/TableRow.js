058bfcfd684619fb899be750a4a0c74dc18c7367
Daniel <daniel.kfir@sage.com>
2021-09-06 17:46:26 +0300

    changes on teacher projects display

--- client/src/components/TableRow/TableRow.js
+++ client/src/components/TableRow/TableRow.js
@@ -1,24 +1,60 @@
 import React from "react";
+import { useHistory } from "react-router";
 import "./TableRow.scss";
 
-const TableRow = ({ project }) => {
+const TableRow = ({ project, linkType }) => {
+  const history = useHistory();
+  const onFieldClick = () => {
+    linkType === "display"
+      ? history.push("/displayproject")
+      : history.push("/editproject");
+  };
+  const statusFormat =
+    project.status === "pendingTeacherApproval"
+      ? "ממתין לאישור מרצה"
+      : project.status === "pendingStudentsEdit"
+      ? "ממתין לעריכת סטודנטים"
+      : "אושר לדף הסדנאות";
   return (
     <div className="content-row">
       <div className="img-wrapper">
         <img src={project.projectImg} className="img-name-content" />
       </div>
       <div className="project-name">
-        <span>{project.projectName}</span>
+        <span className="hover-white" onClick={onFieldClick}>
+          {project.projectName}
+        </span>
       </div>
       <div className="course-content">
-        <span>{project.course}</span>
+        <span className="hover-white" onClick={onFieldClick}>
+          {project.course}
+        </span>
       </div>
       <div className="project-id-content">
-        <span>{project.id}</span>
+        <span className="hover-white" onClick={onFieldClick}>
+          {project.id}
+        </span>
       </div>
-      <div className="status-content">
-        <span>{project.status}</span>
-      </div>
+      {linkType === "edit" && (
+        <div className="last-content">
+          <span
+            className={
+              project.status === "pendingTeacherApproval"
+                ? "orange"
+                : project.status === "pendingStudentsEdit"
+                ? "red"
+                : "green"
+            }
+          >
+            {statusFormat}
+          </span>
+        </div>
+      )}
+      {linkType === "display" && (
+        <div className="last-content">
+          <span className="hover-white">{project.teacherName}</span>
+        </div>
+      )}
     </div>
   );
 };
